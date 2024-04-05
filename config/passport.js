ea4574fe1bb4bb549072596e6c10aa70758fe66a
Blue-pill-786 <113605539+Blue-pill-786@users.noreply.github.com>
2024-04-05 21:28:41 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -25,7 +25,7 @@ module.exports = function(passport) {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "https://authentication-xx4n.onrender.com/home",
     scope: ['profile', 'email']
   }, async function(accessToken, refreshToken, profile, done) {
     try {
